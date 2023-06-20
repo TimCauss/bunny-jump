c30b96a0a14f0d989976c813a0202b996be2b7bb
TimCauss <t.caussignac@gmail.com>
2023-06-20 20:34:44 +0200

    Feat:scrolling effect* Feat:change player collison to bottom only* Feat:replace platforms out of screen* Feat:fix background

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,22 +6,25 @@ export default class Game extends Phaser.Scene {
   }
 
   /** @type {Phaser.Physics.Arcade.Sprite} */
-  player
+  player;
+  /** @type {Phaser.Physics.Arcade.StaticGroup} */
+  platforms;
 
   preload() {
     //Loading the background
     this.load.image("background", "assets/background/bg_layer1.png");
     //loading the platform image
     this.load.image("platform", "assets/platform/ground_grass.png");
-    //preloading player asset
+    //loading player asset
     this.load.image("bunny-stand", "assets/player/bunny1_stand.png");
   }
 
   create() {
-    this.add.image(240, 320, "background");
+    //Creating the background:
+    this.add.image(240, 320, "background").setScrollFactor(1, 0);
 
     //Creating platforms (static, group):
-    const platforms = this.physics.add.staticGroup();
+    this.platforms = this.physics.add.staticGroup();
 
     // then create 5 platforms from the group
     for (let i = 0; i < 5; ++i) {
@@ -29,19 +32,43 @@ export default class Game extends Phaser.Scene {
       const y = 150 * i;
 
       /** @type {Phaser.Physics.Arcade.Sprite} */
-      const platform = platforms.create(x, y, "platform");
+      const platform = this.platforms.create(x, y, "platform");
       platform.scale = 0.5;
 
       /** @type {Phaser.Physics.Arcade.StaticBody} */
       const body = platform.body;
       body.updateFromGameObject();
+    }
+    // Create bunny sprite:
+    this.player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
 
-      // Create bunny sprite:
-      const player = this.physics.add
-        .sprite(240, 320, "bunny-stand")
-        .setScale(0.5);
+    this.physics.add.collider(this.platforms, this.player);
+    this.player.body.checkCollision.up = false;
+    this.player.body.checkCollision.left = false;
+    this.player.body.checkCollision.right = false;
 
-      this.physics.add.collider(platforms, player);
+    this.cameras.main.startFollow(this.player);
+  }
+  update() {
+    this.platforms.children.iterate((child) => {
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = child;
+
+      const scrollY = this.cameras.main.scrollY;
+      if (platform.y >= scrollY + 700) {
+        platform.y = scrollY - Phaser.Math.Between(50, 100);
+        platform.body.updateFromGameObject();
+      }
+    });
+    //find out from Arcade Physics if the player's physics body
+    //is touching something below it
+    const touchingDown = this.player.body.touching.down;
+
+    if (touchingDown) {
+      //this makes the player jump straight up
+      this.player.setVelocityY(-300);
     }
   }
 }
